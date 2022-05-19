907a77c190020caa948ff3d0d89e17fcd0ccb196
Pooja Bhansali <pbhansali05@gmail.com>
2022-05-19 12:55:47 +0800

    created categories model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_19_043731) do
+ActiveRecord::Schema.define(version: 2022_05_19_045032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.boolean "surfboard", default: false
+    t.boolean "fins", default: false
+    t.boolean "leash", default: false
+    t.boolean "covers", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
