8db6a469df798a8d66605f8281f47b89346abff3
Pooja Bhansali <pbhansali05@gmail.com>
2022-05-19 15:30:00 +0800

    added methods in products controller, started on homepage HTML

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,39 @@
 class ProductsController < ApplicationController
+
+  def index
+    @product = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to boats_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :brand, :description, :type, :quantity, :length, :condition, :weight, :price)
+  end
 end
